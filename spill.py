5e0f0bfa8ee254c43c3ea10128e050abd1a3b97f
JonasG <Jonas.gronlien@gmail.com>
2023-11-16 13:28:54 +0100

    begynte på kodingen

--- spill.py
+++ spill.py
@@ -10,19 +10,34 @@ skjerm = pygame.display.set_mode((BREDDE, HOYDE))
 pygame.display.set_caption("skytespill")
 
 
+class soldat(pygame.sprite.Sprite):
+    def __init__(self, x, y, scale):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('img/player/idle/0.png')
+        self.image = pygame.transform.scale(img, (int(img.get_width()* scale), int( img.get_height()* scale)))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+    
+    def draw(self):
+        skjerm.blit(self.image, self.rect)
+    
 
-x = 200
-y = 200
 
-
+spiller = soldat(700, 200, 3)
+spiller2 = soldat(400, 200, 3)
+        
 
 run = True 
 while run: 
 
+    spiller.draw()
+    spiller2.draw()
+
     for event in pygame.event.get():
         #quit game
         if event.type == pygame.QUIT:
-            run : False
-
+            run = False
+    
+    pygame.display.flip()
 
 pygame : quit
